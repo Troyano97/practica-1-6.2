3e68d61c4cee8cbae069954f9e15e00eb9da049f
Troyano97 <ivanferreyradaniel@gmail.com>
2023-04-12 23:04:36 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+let booleano = prompt ("caracter bolaeno");
+let entero = prompt ("numero entero");
+let decimal = prompt ("numero decimal");
+let caracter = prompt ("caracter");
+let palabra = prompt ("palabra");
+
+document.write ("<h1> Caracter booleano: " + booleano + "</h1>");
+document.write ("<h1> Caracter entero: " + entero + "</h1>");
+document.write ("<h1> Caracter decimal: " + decimal + "</h1>");
+document.write ("<h1> Caracter: " + caracter + "</h1>");
+document.write ("<h1> Caracter palabra: " + palabra + "</h1>");
